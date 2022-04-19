d4d0e4e43b175fcba003aee54cef3f48de45363d
Vivek Tomar <86093210+im-vivektomar@users.noreply.github.com>
2022-04-19 16:27:02 +0530

    Create start.py

---
+++ plugins/start.py
@@ -0,0 +1,35 @@
+from pyrogram import Client, filters
+from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
+import humanize
+from helper.database import  insert 
+
+@Client.on_message(filters.private & filters.command(["start"]))
+async def start(client,message):
+	insert(int(message.chat.id))
+	await message.reply_text(text =f"""
+	ʜᴇʟʟᴏ 👋 {message.from_user.first_name }
+	
+➠ ɪ'ᴍ ᴀ ᴛɢ ꜰɪʟᴇ & ᴠɪᴅᴇᴏ ʀᴇɴᴀᴍᴇʀ ʙᴏᴛ
+
+➠ ɪ ᴄᴀɴ ʀᴇɴᴀᴍᴇ ✍️ ᴀɴʏ ꜰɪʟᴇ 🗃️ ᴡɪᴛʜ ᴄᴜꜱᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ ꜱᴜᴘᴘᴏʀᴛ & ʀᴇᴜᴘʟᴏᴀᴅ ɪᴛ ᴛᴏ ᴛɢ ᴀꜱ ᴀ ꜰɪʟᴇ 🗃️ ᴏʀ ᴠɪᴅᴇᴏ 📷
+       
+➠ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ᴡɪᴛʜ ❤️ ʙʏ : @ChVivekTomar
+	""",reply_to_message_id = message.message_id ,  
+	reply_markup=InlineKeyboardMarkup(
+	 [[ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/+4BCzLkyATjswNTA1") ]  ]))
+	
+
+
+
+@Client.on_message(filters.private &( filters.document | filters.audio | filters.video ))
+async def send_doc(client,message):
+       media = await client.get_messages(message.chat.id,message.message_id)
+       file = media.document or media.video or media.audio 
+       filename = file.file_name
+       filesize = humanize.naturalsize(file.file_size)
+       fileid = file.file_id
+       await message.reply_text(
+       f"""__What do you want me to do with this file?__\n**File Name** :- {filename}\n**File Size** :- {filesize}"""
+       ,reply_to_message_id = message.message_id,
+       reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📝 Rename ",callback_data = "rename")
+       ,InlineKeyboardButton("Cancel✖️",callback_data = "cancel")  ]]))
