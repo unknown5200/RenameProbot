eb2698d61b837ba20da687b2c00f80bd20ffd92d
Vivek Tomar <86093210+im-vivektomar@users.noreply.github.com>
2022-05-05 22:20:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import os
 
 TOKEN = os.environ.get("TOKEN", "")
 
-APP_ID = int(os.environ.get("APP_ID", ""))
+API_ID = int(os.environ.get("API_ID", ""))
 
 API_HASH = os.environ.get("API_HASH", "")
 
