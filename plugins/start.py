30c0a519bf6b799fc6927fdcce44fe2c30f505ee
unknown5200 <105126871+unknown5200@users.noreply.github.com>
2022-05-16 09:04:41 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -13,10 +13,9 @@ async def start(client,message):
 
 ➠ 𝘐 𝘊𝘢𝘯 𝘙𝘦𝘯𝘢𝘮𝘦 ✍️ 𝘈𝘯𝘺 𝘍𝘪𝘭𝘦 🗃️ & 𝘝𝘪𝘥𝘦𝘰 📸 𝘞𝘪𝘵𝘩 𝘊𝘶𝘴𝘵𝘰𝘮 𝘛𝘩𝘶𝘮𝘣𝘯𝘢𝘪𝘭 𝘚𝘶𝘱𝘱𝘰𝘳𝘵.
        
-➠ 𝘔𝘢𝘪𝘯𝘵𝘢𝘪𝘯𝘦𝘥 𝘉𝘺 : @ChVivekTomar
+➠ 𝘔𝘢𝘪𝘯𝘵𝘢𝘪𝘯𝘦𝘥 𝘉𝘺 : Anonymous 
 	""",reply_to_message_id = message.message_id ,  
-	reply_markup=InlineKeyboardMarkup(
-	 [[ InlineKeyboardButton("About Me 🇮🇳" ,url="https://t.me/ItsAll_AboutMe") ]  ]))
+	)
 	
 
 
